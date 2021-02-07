813f0cbd1f17c7140d5cb594f47bff21f2a6cbac
Yago Lopes <yagolopeslazaro@gmail.com>
2021-02-07 01:09:50 -0300

    feat: update colors storybook

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,17 +1,27 @@
 import { create } from '@storybook/theming';
 
 export default create({
-  base: 'dark',
-  // colorSecondary: '#FCA311',
-  // appBg: '#ffff',
-  appContentBg: '#8c8c8c',
-  // appBorderColor: '#E5E5E5',
-  // textColor: '#fff',
+  base: 'light',
+  colorPrimary: '#209F85',
+  colorSecondary: '#0E9BEF',
+  appBg: '#454751',
+  fontBase: '"Roboto", sans-serif',
+  fontCode: 'monospace',
+  textColor: '#A7A9AC',
+  textInverseColor: '#A7A9AC',
 
-  // //Toolbar default and active coolors
-  // barTextColor: '#0000',
-  // barSelectedColor: '#fff',
-  // barBg: '#fff',
+  appContentBg: '#454751',
+  appBorderColor: '#209F85',
+  appBorderRadius: 4,
+
+  inputBg: '#fffff',
+  inputBorder: '#209F85',
+  inputTextColor: '#fffff',
+  inputBorderRadius: 4,
+
+  barTextColor: 'silver',
+  barSelectedColor: '#0E9BEF',
+  barBg: '#454751',
   brandTitle: 'UnityForms',
   brandUrl: 'https://github.com/YagoLopes/unityform',
 });
